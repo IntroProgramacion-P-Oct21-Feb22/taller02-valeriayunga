5041a2bef1367310c41f133a51bae581f1e857ba
Valeria Yunga <vayunga@utpl.edu.ec>
2021-10-22 18:54:22 -0500

    primer commit

---
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto01;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Proyecto01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+       // TODO code application logic here
+    System.out.println("Ejemplo básico de Java");
+    System.out.println("Versionado a través de Git");
+       System.out.println("Manejado desde GitHub");
+    }
+    
+    
+}
