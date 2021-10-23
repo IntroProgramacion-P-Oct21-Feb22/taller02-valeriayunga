1496ceb3202f9b90f63e8ea3b284aed53beaed8e
Valeria Yunga <vayunga@utpl.edu.ec>
2021-10-22 19:10:00 -0500

    primer commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -16,9 +16,11 @@ public class Proyecto01 {
     public static void main(String[] args) {
      
        // TODO code application logic here
-    System.out.println("Ejemplo básico de Java");
-    System.out.println("Versionado a través de Git");
-       System.out.println("Manejado desde GitHub");
+    System.out.println("Ejemplo de Java");
+    System.out.println("Manejado desde GitHub");
+       System.out.println("Versionado a través de Git");
+              System.out.println("Creado por <ubicar su usuario de github>");
+
     }
     
     
